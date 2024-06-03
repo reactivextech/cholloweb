a3fb2ce278a08c648050467895c1c7faf4956cc7
Antonio Gallardo <webmaster.jg.51@hotmail.com>
2024-06-03 11:59:00 -0400

    Modificacion de login 01

---
+++ pagos.php
@@ -0,0 +1,86 @@
+<?php 
+session_start();
+
+require_once "ValidacionSesion.php";
+
+if(!$isLoggedIn) {
+    header("Location: ./login");
+}
+?>
+<!doctype html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="icon" href="favicon.ico">
+<title>Login seguro usando PHP Session y Cookies - BaulPHP</title>
+
+<!-- Bootstrap core CSS -->
+<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+<!-- Custom styles for this template -->
+<link href="assets/sticky-footer-navbar.css" rel="stylesheet">
+<style>
+.error-message {
+    text-align: center;
+    color: #FF0000;
+}
+</style>
+</head>
+
+<body>
+<header> 
+  <!-- Fixed navbar -->
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark"> <a class="navbar-brand" href="#">BaulPHP</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active"> <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a> </li>
+      </ul>
+      <form class="form-inline mt-2 mt-md-0">
+        <input class="form-control mr-sm-2" type="text" placeholder="Buscar" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Busqueda</button>
+      </form>
+    </div>
+  </nav>
+</header>
+
+<!-- Begin page content -->
+
+<div class="container">
+  <h3 class="mt-5">Login seguro usando PHP Session y Cookies</h3>
+  <hr>
+  <div class="row">
+    <div class="col-12 col-md-12"> 
+      <!-- Contenido -->
+
+
+<div class="alert alert-primary" role="alert">
+  ¡Has iniciado sesión correctamente!. <a href="Salir.php">Salir</a>
+</div>
+
+      <!-- Fin Contenido --> 
+    </div>
+  </div>
+  <!-- Fin row --> 
+  
+</div>
+<!-- Fin container -->
+<footer class="footer">
+  <div class="container"> <span class="text-muted">
+    <p>Códigos <a href="https://www.baulphp.com/" target="_blank">BaulPHP</a></p>
+    </span> </div>
+</footer>
+<script src="assets/jquery-1.12.4-jquery.min.js"></script> 
+<script src="assets/jquery.validate.min.js"></script> 
+<script src="assets/ValidarRegistro.js"></script> 
+<!-- Bootstrap core JavaScript
+    ================================================== --> 
+<!-- Placed at the end of the document so the pages load faster --> 
+
+<script>window.jQuery || document.write('<script src="assets/js/vendor/jquery-slim.min.js"><\/script>')</script> 
+<script src="assets/js/vendor/popper.min.js"></script> 
+<script src="dist/js/bootstrap.min.js"></script>
+</body>
+</html>
